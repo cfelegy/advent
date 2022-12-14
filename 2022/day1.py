65e18852dbec6c45c92a993ae2844e54c2857fbd
Christopher Felegy <chris@felegy.net>
2022-12-14 17:19:28 -0500

    2022 day 1

---
+++ 2022/day1.py
@@ -0,0 +1,17 @@
+f = open('./inputs/day1_part1', 'r')
+elves = []
+
+elf = 0
+for line in f.readlines():
+    if line == '\n':
+        elves.append(elf)
+        elf = 0
+    else:
+        elf += int(line)
+
+print('part 1: ', max(elves))
+
+elves.sort(reverse=True)
+top3 = sum(elves[0:3])
+
+print('part 2: ', top3)
